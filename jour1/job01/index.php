93a27b76699d4fdb92ede28ab15227e9275c0293
Max <maxime.hadj@laplateforme.io>
2022-03-14 10:23:38 +0100

    commit1

---
+++ jour1/job01/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<meta charset="UTF-8">
+    <head>
+        <script type="text/javascript">alert("Hello Javascript!")</script>
+    </head>
+<body>
+
+</body>
+</html>
